bdef8b2ed9d69b70acc507ea8ed13530644e9919
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-27 09:22:29 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,29 +1,28 @@
-// Code Product Component Here
 import React from 'react';
 import PropTypes from 'prop-types';
-export default class Product extends React.Component{
-  render(){
+
+class Product extends React.Component {
+
+  render() {
     return (
-      <div>
-        <h1>{this.props.name}</h1>
-        <p>{this.props.producer}</p>
-        <p>{this.props.hasWatermark}</p>
-        <p>{this.props.color}</p>
-        <p>{this.props.weight}</p>
-      </div>
+      <div>Product</div>
     )
   }
 }
 
 Product.defaultProps = {
   hasWatermark: false
-};
+}
 
 Product.propTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
-  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  color: PropTypes.oneOf([
+    'white', 
+    'eggshell-white', 
+    'salmon'
+  ]).isRequired,
   weight: (props, propName) => {
     const weight = props[propName];
 
@@ -41,4 +40,6 @@ Product.propTypes = {
       return new Error('The `weight` prop should range between 80 and 300.');
     }
   },
-};
+}
+
+export default Product;
